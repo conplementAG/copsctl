73a7ed9199eac50f66d17480be68797543989001
Denis Biondic <denis.biondic@conplement.de>
2020-03-26 12:03:58 +0100

    feat: version bump

--- cmd/copsctl/root.go
+++ cmd/copsctl/root.go
@@ -22,10 +22,10 @@ var rootCmd = &cobra.Command{
      \___\___/| .__/|___/\___|\__|_|
               |_|                       by Conplement AG
 	
-	Version 0.2.7
+	Version 0.3.0
     `,
 
-	Version: "0.2.7",
+	Version: "0.3.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
