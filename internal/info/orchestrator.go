232f2a85dc036956f2c113070488daf160ee82c2
Christian Gottinger <christian.gottinger@conplement.de>
2023-05-26 08:25:59 +0200

    feat(info): create environment and cluster info commands

--- internal/info/orchestrator.go
+++ internal/info/orchestrator.go
@@ -1,4 +1,4 @@
-package cluster_info
+package info
 
 import (
 	"encoding/json"
@@ -22,28 +22,35 @@ func New(hq hq.HQ) *Orchestrator {
 		executor: hq.GetExecutor(),
 	}
 }
-
 func (o *Orchestrator) ShowClusterInfo() {
+	o.showInfo(Cluster)
+}
+
+func (o *Orchestrator) ShowEnvironmentInfo() {
+	o.showInfo(Environment)
+}
+
+func (o *Orchestrator) showInfo(typeName string) {
 	printConfigSilenceEverythingElse := viper.GetBool(flags.PrintToStdoutSilenceEverythingElse)
 
 	if !printConfigSilenceEverythingElse {
-		logrus.Info("Reading the cluster info ...")
+		logrus.Infof("Reading the %s info ...", typeName)
 		logrus.Info("NOTE: you can use the " + flags.PrintToStdoutSilenceEverythingElse + " flag to silence these outputs (useful for automation)")
 
 		logrus.Info("===========================================================")
-		logrus.Info("==================== Cluster Info:  =======================")
+		logrus.Infof("==================== %s Info:  =======================", typeName)
 		logrus.Info("===========================================================")
 	}
 
-	result, err := o.executor.Execute("kubectl get configmap -n coreops-public -o jsonpath=\"{.data['info\\.json']}\" coreops-cluster-info")
+	result, err := o.executor.Execute(fmt.Sprintf("kubectl get configmap -n coreops-public -o jsonpath=\"{.data['%s-info\\.json']}\" coreops-info", typeName))
 
 	if err != nil {
 		logrus.Errorf(err.Error())
 		panic(err)
 	}
 
-	result = strings.TrimPrefix(result, "'")
-	result = strings.TrimSuffix(result, "'")
+	result = strings.Trim(result, "'")
+	result = strings.Trim(result, "\"")
 
 	if printConfigSilenceEverythingElse {
 		// no pretty formats or anything is needed if printing for parse purposes
@@ -64,3 +71,8 @@ func (o *Orchestrator) ShowClusterInfo() {
 		fmt.Println(string(indented))
 	}
 }
+
+const (
+	Cluster     string = "cluster"
+	Environment        = "environment"
+)
